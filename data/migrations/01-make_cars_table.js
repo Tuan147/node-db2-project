7b7089eb1904636b27958cb4c7dab1f4b4cb917a
unknown <tuan5301@gmail.com>
2022-02-15 14:14:06 -0500

    added in all cars schema using knex

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.table('cars', (tbl) => {
+    tbl.increment();
+    tbl.string('vin', 128).unique().notNullable();
+    tbl.string('make', 128).notNullable();
+    tbl.string('model', 128).notNullable();
+    tbl.numeric('mileage', 128).notNullable();
+    tbl.string('title', 128);
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
